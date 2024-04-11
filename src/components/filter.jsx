2b2c6dec4ede64a8dc560c82d70bf1fcda9c68f4
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2024-04-12 00:06:13 +0900

    Bismillah: Shopping-List Ui/Ux

---
+++ src/components/filter.jsx
@@ -0,0 +1,13 @@
+function Filter() {
+	return (
+		<div className='filter'>
+			<div className='btns'>
+				<button>All products</button>
+				<button className='active'>Conpleted</button>
+				<button>Big size</button>
+			</div>
+		</div>
+	)
+}
+
+export default Filter
